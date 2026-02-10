ab3194d9438f6d1218b2efe146be67f13380a5b0
lucasghidini <lucasdemattos1306@gmail.com>
2026-02-10 18:26:26 -0300

    first commit

---
+++ dags/1.PrimeiraDag.py
@@ -0,0 +1,17 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+	dag_id='primeira_dag',
+	description= 'Nossa primeira Dag',
+	schedule=None,
+	start_date=pendulum.datetime(2025,1,1, tz='America/Sao_Paulo'),
+	catchup=False,
+	tags=['curso','exemplo'],
+) as dag:
+	task1= BashOperator(task_id='tsk1', bash_command='sleep 5')
+	task2= BashOperator(task_id='tsk2', bash_command='sleep 5')
+	task3= BashOperator(task_id='tsk3', bash_command='sleep 5')
+	
+	task1 >> task2 >> task3
