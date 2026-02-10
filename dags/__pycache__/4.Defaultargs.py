9f9ececbf3e5287433c52d9c0a30df1f064b3110
lucasghidini <lucasdemattos1306@gmail.com>
2026-02-10 18:39:23 -0300

    Defalut_args testes

--- dags/__pycache__/4.Defaultargs.py
+++ dags/__pycache__/4.Defaultargs.py
@@ -1,10 +1,22 @@
 import pendulum
 from airflow import DAG
+from datetime import timedelta
 from airflow.operators.bash import BashOperator
 
+default_args = {
+    'depends_on_past': False,
+	'email': ['teste@gmail.com'],
+	'email_on_failure': False,
+    'email_on_retry': False,
+	'retries': 1,
+	'retry_delay': timedelta(seconds=10)
+	
+}
+
 with DAG(
-	dag_id='primeira_dag',
-	description= 'Nossa primeira Dag',
+	dag_id='default_args',
+	description= 'Dag teste default args',
+	default_args=default_args,
 	schedule=None,
 	start_date=pendulum.datetime(2025,1,1, tz='America/Sao_Paulo'),
 	catchup=False,
@@ -14,4 +26,4 @@ with DAG(
 	task2= BashOperator(task_id='tsk2', bash_command='sleep 5')
     task3= BashOperator(task_id='tsk3', bash_command='sleep 5')
 	
-	task1 >> [task2,  task3]
+	task1 >> task2 >> task3
