cb8850ba7861ed22bdabf685cc4e9c00928bed9b
lucasghidini <lucasdemattos1306@gmail.com>
2026-02-10 18:29:37 -0300

    Tasks com paralelismo

--- dags/3.TerceiraDag.py
+++ dags/3.TerceiraDag.py
@@ -3,16 +3,15 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 with DAG(
-	dag_id='segunda_dag',
-	description= 'Nossa segunda Dag',
+	dag_id='primeira_dag',
+	description= 'Nossa primeira Dag',
 	schedule=None,
 	start_date=pendulum.datetime(2025,1,1, tz='America/Sao_Paulo'),
 	catchup=False,
 	tags=['curso','exemplo'],
 ) as dag:
 	task1= BashOperator(task_id='tsk1', bash_command='sleep 5')
-	# task2= BashOperator(task_id='tsk2', bash_command='sleep 5')
-	task2= BashOperator(task_id='tsk2', bash_command='text1')
+	task2= BashOperator(task_id='tsk2', bash_command='sleep 5')
     task3= BashOperator(task_id='tsk3', bash_command='sleep 5')
 	
-	task1 >> task2 >> task3
+	task1 >> [task2,  ask3]
